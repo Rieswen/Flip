af648bb7798baaee30502e9d7cf7cac289bb8d39
Rares <67231221+Rieswen@users.noreply.github.com>
2020-11-15 15:56:44 +0200

    Add files via upload

---
+++ Flip.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class FlipShit : MonoBehaviour
+{
+    private bool FacingRight = true;
+    private float moveInput;
+
+    void Start()
+    {
+    }
+    void Flip()
+    {
+        FacingRight = !FacingRight;
+        Vector3 Scaler = transform.localScale;
+        transform.localScale = Scaler;
+        Scaler.x *= -1;
+    }
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+    }
+
+
+    void Update()
+    {
+        if (moveInput > 0)
+        {
+            Flip();
+        }
+        else if (moveInput < 0)
+        {
+            Flip();
+        }
+    }
+}
